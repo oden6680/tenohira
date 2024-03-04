d2e2074fe1dfd17da0c3529c9bc8b33b866f2f5f
Naoya Oda <oden6680@gmail.com>
2024-03-05 05:47:57 +0900

    種類別に色を分けて表示する処理

--- components/Map.jsx
+++ components/Map.jsx
@@ -3,11 +3,44 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "./Map.css";
 import L from "leaflet";
 import TradisionalClaftsData from "../TraditionalCrafts.json";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
-L.Icon.Default.imagePath = "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/";
+//defaultMarker
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+const colorMarker = (color) => {
+  return L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    className: `default-marker ${color}`,
+  });
+};
+
+const categories = {
+  stonework: "light-blue",
+  ceramics: "aqua",
+  dyedproducts: "green",
+  craftmaterials: "lime-green",
+  dolls: "light-yellow",
+  buddhist: "yellow",
+  metalwork: "gold",
+  dyedwovenproducts: "orange",
+  stationery: "red-orange",
+  paper: "red",
+  woodwork: "dark-pink",
+  lacquerware: "pink",
+  preciousstonework: "magenta",
+  fabric: "purple",
+  other: "deep-purple",
+};
 
 export const Map = () => {
-  const position = [35.500, 136.500];
+  const position = [35.5, 136.5];
   const zoom = 5;
   return (
     <MapContainer center={position} zoom={zoom}>
@@ -22,6 +55,7 @@ export const Map = () => {
             craft.geometry.coordinates[1],
             craft.geometry.coordinates[0],
           ]}
+          icon={colorMarker(categories[craft.properties.category])}
         >
           <Popup>
             {craft.properties.name} <br /> {craft.properties.overview}
