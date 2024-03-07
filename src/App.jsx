800faa775910f1400f63524083911b00c6396acd
Naoya Oda <oden6680@gmail.com>
2024-03-07 21:19:21 +0900

    ミーティング時の内容を反映

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Footer from "./components/Footer";
 import Map from "./components/Map";
 import About from "./components/About";
 import Infomation from "./components/Infomation";
-import Photo from "./components/Photo";
 import SocialLinks from "./components/SocialLinks";
 import "leaflet/dist/leaflet.css";
 import { CssBaseline, Box } from "@mui/material";
@@ -16,10 +15,9 @@ export default function App() {
       <Header />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Routes>
-          <Route path="/" element={<Map />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/" element={<About />} />
+          <Route path="/map" element={<Map />} />
           <Route path="/infomation" element={<Infomation />} />
-          <Route path="/photo" element={<Photo />} />
           <Route path="/sociallinks" element={<SocialLinks />} />
         </Routes>
       </Box>
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,31 +3,25 @@ import { Link as RouterLink } from "react-router-dom";
 import { IconButton, Tooltip } from "@mui/material";
 import MapIcon from "@mui/icons-material/Map";
 import GroupsIcon from "@mui/icons-material/Groups";
-import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
 import InfoIcon from "@mui/icons-material/Info";
 import LinkIcon from "@mui/icons-material/Link";
 
 export default function NavigationBar() {
   return (
     <React.Fragment>
-      <Tooltip title="伝統工芸マップ">
+      <Tooltip title="「手のひらに和を」とは">
         <IconButton color="inherit" component={RouterLink} to="/">
-          <MapIcon />
+          <GroupsIcon />
         </IconButton>
       </Tooltip>
-      <Tooltip title="「手のひらに和を」とは">
-        <IconButton color="inherit" component={RouterLink} to="/about">
-          <GroupsIcon />
+      <Tooltip title="伝統工芸マップ">
+        <IconButton color="inherit" component={RouterLink} to="/map">
+          <MapIcon />
         </IconButton>
       </Tooltip>
       <Tooltip title="お知らせ">
         <IconButton color="inherit" component={RouterLink} to="/infomation">
           <InfoIcon />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title="写真">
-        <IconButton color="inherit" component={RouterLink} to="/photo">
-          <InsertPhotoIcon />
         </IconButton>
       </Tooltip>
       <Tooltip title="SNS">
